14b7b4a37255e0401daf64be68c9c32402854ff4
Sergey-Akmalov <akmolgt@gmail.com>
2017-07-31 20:49:36 +0300

    reconfigure components using service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from "@angular/core";
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is simply a test',
             'https://s-media-cache-ak0.pinimg.com/originals/f1/60/14/f1601412ddf7274ab46c5f43062979ad.jpg'),
