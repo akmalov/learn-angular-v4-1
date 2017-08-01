22aa12abd2bc7a87a7ceb5fea7ecd55c9aa777fe
Sergey-Akmalov <akmolgt@gmail.com>
2017-08-01 14:16:10 +0300

    passing ingredients from recipes to the shopping list via service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,17 +1,36 @@
-import { EventEmitter } from "@angular/core";
+import {EventEmitter, Injectable} from "@angular/core";
 import { Recipe } from './recipe.model';
+import { Ingredient } from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test',
-            'https://s-media-cache-ak0.pinimg.com/originals/f1/60/14/f1601412ddf7274ab46c5f43062979ad.jpg'),
-        new Recipe('Another Test Recipe', 'This is simply a test',
-            'https://s-media-cache-ak0.pinimg.com/originals/f1/60/14/f1601412ddf7274ab46c5f43062979ad.jpg')
+        new Recipe('Tasty Schnitzel',
+            'A super-tasty Schnitzel - just awesome!',
+            'http://kmetro.restaurantden.com/wp-content/uploads/sites/67/2016/03/dish_1.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]),
+        new Recipe('Big Fat Burger',
+            'What else you need to say?',
+            'http://bk-emea-prd.s3.amazonaws.com/sites/burgerking.co.uk/files/BK_Angus_Burger_500X540px.png',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1)
+            ])
     ];
+
+    constructor(private slService: ShoppingListService) {}
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]){
+        this.slService.addIngredients(ingredients);
     }
 }
